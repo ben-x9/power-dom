06a9788521f88f49be36be6b86ba02b0d793bb64
Ben Lambert <ben@joyxer.com>
2017-07-13 14:12:58 +0900

    fix bug with a tag

--- src/index.ts
+++ src/index.ts
@@ -84,8 +84,9 @@ export const li = tag("li")
 export const input = tag("input")
 export const textarea = tag("textarea")
 export const img = tag("img")
+export const a = tag("a")
+
 export const br = h("br")
-export const a = h("a")
 
 const svgTag = (type: string) => {
   const t = tag(type)
